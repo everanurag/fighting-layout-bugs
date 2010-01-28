45dcfe41841b1e9101e46859b622853fabb9d81e
michael.tamm2 <michael.tamm2@eba416d6-9927-11de-92e6-d3aed0da6f53>
2010-01-28 13:09:23 +0000

    Added DetectNeedsHorizontalScrolling to FightingLayoutBugs class, made FightingLayoutBugs class configurable
    git-svn-id: https://fighting-layout-bugs.googlecode.com/svn/trunk@52 eba416d6-9927-11de-92e6-d3aed0da6f53

--- src/main/java/de/michaeltamm/fightinglayoutbugs/FightingLayoutBugs.java
+++ src/main/java/de/michaeltamm/fightinglayoutbugs/FightingLayoutBugs.java
@@ -18,11 +18,14 @@ package de.michaeltamm.fightinglayoutbugs;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Finds layout bugs in a web page by applying all available
  * layout bug detectors to it. These are currently:
  * <ul>
+ *     <li>{@link DetectNeedsHorizontalScrolling}</li>
  *     <li>{@link DetectInvalidImageUrls}</li>
  *     <li>{@link DetectTextNearOrOverlappingHorizontalEdge}</li>
  *     <li>{@link DetectTextNearOrOverlappingVerticalEdge}</li>
@@ -33,26 +36,55 @@ import java.util.Collection;
  */
 public class FightingLayoutBugs extends AbstractLayoutBugDetector {
 
+    private final Map<Class<? extends LayoutBugDetector>, LayoutBugDetector> _layoutBugDetectors;
+
+    private TextDetector _textDetector;
+    private EdgeDetector _edgeDetector;
+
+    public FightingLayoutBugs() {
+        _layoutBugDetectors = new LinkedHashMap<Class<? extends LayoutBugDetector>, LayoutBugDetector>();
+        // The first detector should be DetectNeedsHorizontalScrolling, because it resizes the browser window ...  
+        _layoutBugDetectors.put(DetectNeedsHorizontalScrolling.class, new DetectNeedsHorizontalScrolling());
+        _layoutBugDetectors.put(DetectInvalidImageUrls.class, new DetectInvalidImageUrls());
+        _layoutBugDetectors.put(DetectTextNearOrOverlappingHorizontalEdge.class, new DetectTextNearOrOverlappingHorizontalEdge());
+        _layoutBugDetectors.put(DetectTextNearOrOverlappingVerticalEdge.class, new DetectTextNearOrOverlappingVerticalEdge());
+        _layoutBugDetectors.put(DetectTextWithTooLowContrast.class, new DetectTextWithTooLowContrast());
+    }
+
+    /**
+     * Sets the {@link TextDetector} to use.
+     */
+    public void setTextDetector(TextDetector textDetector) {
+        _textDetector = textDetector;
+    }
+
+    /**
+     * Sets the {@link EdgeDetector} to use.
+     */
+    public void setEdgeDetector(EdgeDetector edgeDetector) {
+        _edgeDetector = edgeDetector;
+    }
+
+    /**
+     * Call this method to gain access to one of the {@link LayoutBugDetector}s
+     * for calling setter methods on it.
+     */
+    public <D extends LayoutBugDetector> D configure(Class<D> detectorClass) {
+        return (D) _layoutBugDetectors.get(detectorClass);
+    }
+
     public Collection<LayoutBug> findLayoutBugsIn(WebPage webPage) throws Exception {
+        if (_textDetector != null) {
+            webPage.setTextDetector(_textDetector);
+        }
+        if (_edgeDetector != null) {
+            webPage.setEdgeDetector(_edgeDetector);
+        }
         final Collection<LayoutBug> result = new ArrayList<LayoutBug>();
-        LayoutBugDetector layoutBugDetector;
-
-        layoutBugDetector = new DetectInvalidImageUrls();
-        layoutBugDetector.setScreenshotDir(_screenshotDir);
-        result.addAll(layoutBugDetector.findLayoutBugsIn(webPage));
-
-        layoutBugDetector = new DetectTextNearOrOverlappingHorizontalEdge();
-        layoutBugDetector.setScreenshotDir(_screenshotDir);
-        result.addAll(layoutBugDetector.findLayoutBugsIn(webPage));
-
-        layoutBugDetector = new DetectTextNearOrOverlappingVerticalEdge();
-        layoutBugDetector.setScreenshotDir(_screenshotDir);
-        result.addAll(layoutBugDetector.findLayoutBugsIn(webPage));
-
-        layoutBugDetector = new DetectTextWithTooLowContrast();
-        layoutBugDetector.setScreenshotDir(_screenshotDir);
-        result.addAll(layoutBugDetector.findLayoutBugsIn(webPage));
-
+        for (LayoutBugDetector detector : _layoutBugDetectors.values()) {
+            detector.setScreenshotDir(_screenshotDir);
+            result.addAll(detector.findLayoutBugsIn(webPage));
+        }
         return result;
     }
 }
