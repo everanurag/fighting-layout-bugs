25c45af9dbe51061794f37d29d0938a4ae7c32a8
michael.tamm2 <michael.tamm2@eba416d6-9927-11de-92e6-d3aed0da6f53>
2011-03-28 12:04:59 +0000

    Simplified algorithm of SimpleEdgeDetector, SimpleEdgeDetector uses WCAG 2.0 formula for contrast now too
    git-svn-id: https://fighting-layout-bugs.googlecode.com/svn/trunk@87 eba416d6-9927-11de-92e6-d3aed0da6f53

--- src/main/java/de/michaeltamm/fightinglayoutbugs/SimpleEdgeDetector.java
+++ src/main/java/de/michaeltamm/fightinglayoutbugs/SimpleEdgeDetector.java
@@ -16,227 +16,180 @@
 
 package de.michaeltamm.fightinglayoutbugs;
 
+import static de.michaeltamm.fightinglayoutbugs.ImageHelper.getContrast;
 import static de.michaeltamm.fightinglayoutbugs.Screenshot.withNoText;
+import static de.michaeltamm.fightinglayoutbugs.StringHelper.amountString;
 
 /**
  * <p>Detects horizontal and vertical edges with a simple algorithm.</p>
  * For horizontal edges:<ol>
- *     <li>candidates = horizontal pixel sequences of the same or {@link #setSimilarColorsMaxRgbSqrDistance similar colors} with a certain {@link #setMinEdgeLength minimal length}</li>
- *     <li>Discard all candidate pixels, which don't have a {@link #setEdgeMinRgbSqrDistance high contrast} to the line above or below</li>
- *     <li>Discard all remaining pixel sequences, which don't have a certain {@link #setMinEdgeLength minimal length}
+ *     <li>candidates = all pixels which have a {@link #setEdgeMinContrast high contrast} to the pixel below/above.
+ *     <li>edges = all horizontal pixel sequences in the candidates of {@link #setSimilarColorMaxContrast similar color}
+ *         and with a certain {@link #setMinHorizontalEdgeLength minimal length}
  * </ol>
- * For vertical edges:<ol>
- *     <li>candidates = vertical pixel sequences of the same or {@link #setSimilarColorsMaxRgbSqrDistance similar colors} with a certain {@link #setMinEdgeLength minimal length}</li>
- *     <li>Discard all candidate pixels, which don't have a {@link #setEdgeMinRgbSqrDistance high contrast} to the column at the left or at the right</li>
- *     <li>Discard all remaining pixel sequences, which don't have a certain {@link #setMinEdgeLength minimal length}
+ * For horizontal edges:<ol>
+ *     <li>candidates = all pixels which have a {@link #setEdgeMinContrast high contrast} to the pixel on the left/right.
+ *     <li>edges = all vertical pixel sequences in the candidates of {@link #setSimilarColorMaxContrast similar color}
+ *         and with a certain {@link #setMinVerticalEdgeLength minimal length}
  * </ol>
  *
  * @author Michael Tamm
  */
 public class SimpleEdgeDetector implements EdgeDetector {
 
-    private int _minEdgeLength = 16;
-    private int _similarColorsMaxRgbSqrDistance = 30;
-    private int _edgeMinRgbSqrDistance = 900;
+    private double _edgeMinContrast = 1.5;
+    private double _similarColorMaxContrast = 1.5;
+    private int _minHorizontalEdgeLength = 16;
+    private int _minVerticalEdgeLength = 10;
 
     /**
-     * Sets the minimal length for detected edges in pixels, default is <code>16</code>.
+     * Sets the minimal contrast two pixels must have to be considered as candidates for an edge, default is <code>1&#46;75</code>
+     * -- see <a href="http://trace.wisc.edu/contrast-ratio-examples/TextSamples_6x6x6_On_000000.htm">Web-safe Colored Text shown on #000</a>
+     * for a visualization of different contrast values.
      */
-    public void setMinEdgeLength(int length) {
-        _minEdgeLength = length;
+    public void setEdgeMinContrast(double edgeMinContrast) {
+        _edgeMinContrast = edgeMinContrast;
+    }
+
+    public double getEdgeMinContrast() {
+        return _edgeMinContrast;
     }
 
     /**
-     * Sets the maximal {@link #getRgbSqrDistance distance} for two colors to be considered similar, default is 30.
+     * Sets the maximal contrast two pixels can have to be considered as having similar colors, default is <code>1&#46;5</code>
+     * -- see <a href="http://trace.wisc.edu/contrast-ratio-examples/TextSamples_6x6x6_On_000000.htm">Web-safe Colored Text shown on #000</a>
+     * for a visualization of different contrast values.
      */
-    public void setSimilarColorsMaxRgbSqrDistance(int rgbSqrDistance) {
-        _similarColorsMaxRgbSqrDistance = rgbSqrDistance;
+    public void setSimilarColorMaxContrast(double similarColorMaxContrast) {
+        _similarColorMaxContrast = similarColorMaxContrast;
+    }
+
+    public double getSimilarColorMaxContrast() {
+        return _similarColorMaxContrast;
     }
 
     /**
-     * Sets the minimal {@link #getRgbSqrDistance distance} for two colors to be considered of having such a high contrast,
-     * that they might belong to an edge, default is 900.
+     * Sets the minimal length for detected horizontal edges, default is <code>16</code>.
      */
-    public void setEdgeMinRgbSqrDistance(int rgbSqrDistance) {
-        _edgeMinRgbSqrDistance = rgbSqrDistance;
+    public void setMinHorizontalEdgeLength(int minHorizontalEdgeLength) {
+        _minHorizontalEdgeLength = minHorizontalEdgeLength;
+    }
+
+    public int getMinHorizontalEdgeLength() {
+        return _minHorizontalEdgeLength;
+    }
+
+    /**
+     * Sets the minimal length for detected vertical edges, default is <code>10</code>.
+     */
+    public void setMinVerticalEdgeLength(int minVerticalEdgeLength) {
+        _minVerticalEdgeLength = minVerticalEdgeLength;
+    }
+
+    public int getMinVerticalEdgeLength() {
+        return _minVerticalEdgeLength;
     }
 
     public boolean[][] detectHorizontalEdgesIn(WebPage webPage) {
         final int[][] pixels = webPage.getScreenshot(withNoText()).pixels;
         final int w = pixels.length;
         final int h = pixels[0].length;
-        // Detect horizontal pixel sequences of similar color ...
-        final boolean[][] candidates = new boolean[w][h];
-        for (int y = 0; y < h; ++y) {
-            int start = 0;
-            int c = pixels[0][y];
-            int r = (c & 0xFF0000) >> 16;
-            int g = (c & 0xFF00) >> 8;
-            int b = (c & 0xFF);
-            for (int x = 1; x <= w; ++x) {
-                if (x == w || getRgbSqrDistance(r, g, b, (c = pixels[x][y])) > _similarColorsMaxRgbSqrDistance) {
-                    if (x - start >= _minEdgeLength) {
-                        // All pixels from pixels[start][y] to pixels[x - 1][y] have similar color ...
-                        for (int i = start; i < x; ++i) {
-                            candidates[i][y] = true;
-                        }
-                    }
-                    if (x < w) {
-                        start = x;
-                        r = (c & 0xFF0000) >> 16;
-                        g = (c & 0xFF00) >> 8;
-                        b = (c & 0xFF);
-                    }
-                } else {
-                    r = (c & 0xFF0000) >> 16;
-                    g = (c & 0xFF00) >> 8;
-                    b = (c & 0xFF);
-                }
-            }
-        }
-        Visualization.algorithmStepFinished("1.) candidates = horizontal pixel sequences of similar color", candidates);
-        final boolean[][] result = new boolean[w][h];
-        // Detect horizontal edges by comparing candidates with pixel above and below ...
+        // 1.) Determine candidates (those pixels, which have a high contrast to the pixel below/above itself) ...
         final int h1 = h - 1;
-        for (int y = 0; y < h; ++y) {
+        boolean[][] candidates = new boolean[w][h];
+        for (int y = 0; y < h1; ++y) {
             for (int x = 0; x < w; ++x) {
-                if (candidates[x][y]) {
-                    if (y > 0 && getRgbSqrDistance(pixels[x][y - 1], pixels[x][y]) > _edgeMinRgbSqrDistance) {
-                        result[x][y - 1] = true;
-                        result[x][y] = true;
-                    }
-                    if (y < h1 && getRgbSqrDistance(pixels[x][y], pixels[x][y + 1]) > _edgeMinRgbSqrDistance) {
-                        result[x][y] = true;
-                        result[x][y + 1] = true;
-                    }
+                if (haveHighContrast(pixels[x][y], pixels[x][y + 1])) {
+                    candidates[x][y] = true;
+                    candidates[x][y + 1] = true;
                 }
             }
         }
-        Visualization.algorithmStepFinished("2.) Detect horizontal edges by comparing candidates with pixels above and below", result);
-        // Ignore detected horizontal edges which are not long enough ...
+        Visualization.algorithmStepFinished("1.) Determine candidates (those pixels, which have a high contrast to the pixel below/above itself).", candidates);
+        // 2.) Find horizontal pixels sequences in candidates of similar color with configured minimal length ...
+        boolean[][] horizontalEdges = new boolean[w][h];
         for (int y = 0; y < h; ++y) {
             int x1 = 0;
             do {
-                while (x1 < w && !result[x1][y]) {
+                // find first candidate pixel ...
+                while (x1 < w && !candidates[x1][y]) {
                     ++x1;
                 }
                 if (x1 < w) {
+                    // find end of horizontal edge ...
+                    int p1 = pixels[x1][y];
                     int x2 = x1 + 1;
-                    while (x2 < w && result[x2][y]) {
+                    int p2;
+                    while (x2 < w && candidates[x2][y] && haveSimilarColor(p1, p2 = pixels[x2][y])) {
+                        p1 = p2;
                         ++x2;
                     }
-                    if (x2 - x1 < _minEdgeLength) {
-                        for (int i = x1; i < x2; ++i) {
-                            result[i][y] = false;
+                    if (x2 - x1 >= _minHorizontalEdgeLength) {
+                        for (int x = x1; x < x2; ++x) {
+                            horizontalEdges[x][y] = true;
                         }
                     }
                     x1 = x2;
                 }
             } while(x1 < w);
         }
-        Visualization.algorithmFinished("3.) Ignore detected horizontal edges which are not long enough", result);
-        return result;
+        Visualization.algorithmFinished("2.) Find horizontal pixels sequences in candidates of similar color with minimal " + amountString(_minHorizontalEdgeLength, "pixel") + " length.", horizontalEdges);
+        return horizontalEdges;
     }
 
     public boolean[][] detectVerticalEdgesIn(WebPage webPage) {
-        // Take screenshot ...
         final int[][] pixels = webPage.getScreenshot(withNoText()).pixels;
         final int w = pixels.length;
         final int h = pixels[0].length;
-        // Detect vertical pixel sequences of similar color ...
-        final boolean[][] candidates = new boolean[w][h];
-        for (int x = 0; x < w; ++x) {
-            int start = 0;
-            int c = pixels[x][0];
-            int r = (c & 0xFF0000) >> 16;
-            int g = (c & 0xFF00) >> 8;
-            int b = (c & 0xFF);
-            for (int y = 1; y <= h; ++y) {
-                if (y == h || getRgbSqrDistance(r, g, b, (c = pixels[x][y])) > _similarColorsMaxRgbSqrDistance) {
-                    if (y - start >= _minEdgeLength) {
-                        // All pixels from pixels[x][start] to pixels[x][y - 1] have similar color ...
-                        for (int i = start; i < y; ++i) {
-                            candidates[x][i] = true;
-                        }
-                    }
-                    if (y < h) {
-                        start = y;
-                        r = (c & 0xFF0000) >> 16;
-                        g = (c & 0xFF00) >> 8;
-                        b = (c & 0xFF);
-                    }
-                } else {
-                    r = (c & 0xFF0000) >> 16;
-                    g = (c & 0xFF00) >> 8;
-                    b = (c & 0xFF);
-                }
-            }
-        }
-        Visualization.algorithmStepFinished("1.) candidates = vertical pixel sequences of similar color", candidates);
-        final boolean[][] result = new boolean[w][h];
-        // Detect vertical edges ...
+        // 1.) Determine candidates (those pixels, which have a high contrast to the pixel on the left/right) ...
         final int w1 = w - 1;
-        for (int x = 0; x < w; ++x) {
+        boolean[][] candidates = new boolean[w][h];
+        for (int x = 0; x < w1; ++x) {
             for (int y = 0; y < h; ++y) {
-                if (candidates[x][y]) {
-                    if (x > 0 && getRgbSqrDistance(pixels[x - 1][y], pixels[x][y]) > _edgeMinRgbSqrDistance) {
-                        result[x - 1][y] = true;
-                        result[x][y] = true;
-                    }
-                    if (x < w1 && getRgbSqrDistance(pixels[x][y], pixels[x + 1][y]) > _edgeMinRgbSqrDistance) {
-                        result[x][y] = true;
-                        result[x + 1][y] = true;
-                    }
+                if (haveHighContrast(pixels[x][y], pixels[x + 1][y])) {
+                    candidates[x][y] = true;
+                    candidates[x + 1][1] = true;
                 }
             }
         }
-        Visualization.algorithmStepFinished("2.) Detect vertical edges by comparing candidates with pixels on left and right side", result);
-        // Ignore detected vertical edges which are not long enough ...
+        Visualization.algorithmStepFinished("1.) Determine candidates (those pixels, which have a high contrast to the pixel on the left/right).", candidates);
+        // 2.) Find vertical pixels sequences in candidates of similar color and with configured minimal length ...
+        boolean[][] verticalEdges = new boolean[w][h];
         for (int x = 0; x < w; ++x) {
             int y1 = 0;
             do {
-                while (y1 < h && !result[x][y1]) {
+                // find first candidate pixel ...
+                while (y1 < h && !candidates[x][y1]) {
                     ++y1;
                 }
                 if (y1 < h) {
+                    // find end of vertical edge ...
+                    int p1 = pixels[x][y1];
                     int y2 = y1 + 1;
-                    while (y2 < h && result[x][y2]) {
+                    int p2;
+                    while (y2 < h && candidates[x][y2] && haveSimilarColor(p1, p2 = pixels[x][y2])) {
+                        p1 = p2;
                         ++y2;
                     }
-                    if (y2 - y1 < _minEdgeLength) {
-                        for (int i = y1; i < y2; ++i) {
-                            result[x][i] = false;
+                    if (y2 - y1 >= _minVerticalEdgeLength) {
+                        for (int y = y1; y < y2; ++y) {
+                            verticalEdges[x][y] = true;
                         }
                     }
                     y1 = y2;
                 }
             } while(y1 < h);
         }
-        Visualization.algorithmFinished("3.) Ignore detected vertical edges which are not long enough", result);
-        return result;
+        Visualization.algorithmFinished("2.) Find vertical pixels sequences in candidates of similar color and with minimal " + amountString(_minVerticalEdgeLength, "pixel") + " length.", verticalEdges);
+        return verticalEdges;
     }
 
-    /**
-     * Calculates the squared distance between the colors of two pixels in the RGB cube.
-     * The exact formula is: <code>dr * dr + dg * dg + db * db</code> whereby <code>dr</code>
-     * is the delta of red color component of the two given pixels (in the range 0 ... 255),
-     * <code>dg</code> the delta of the green color component and <code>db</code> the delta
-     * of the blue color component.
-     */
-    public static int getRgbSqrDistance(int pixel1, int pixel2) {
-        int r = (pixel1 & 0xFF0000) >> 16;
-        int g = (pixel1 & 0xFF00) >> 8;
-        int b = (pixel1 & 0xFF);
-        r -= (pixel2 & 0xFF0000) >> 16;
-        g -= (pixel2 & 0xFF00) >> 8;
-        b -= (pixel2 & 0xFF);
-        return r * r + g * g + b * b;
+    private boolean haveHighContrast(int rgb1, int rgb2) {
+        return (rgb1 != rgb2) && (getContrast(rgb1, rgb2) >= _edgeMinContrast);
     }
 
-    private static int getRgbSqrDistance(int r1, int g1, int b1, int pixel2) {
-        final int dr = r1 - ((pixel2 & 0xFF0000) >> 16);
-        final int dg = g1 - ((pixel2 & 0xFF00) >> 8);
-        final int db = b1 - ((pixel2 & 0xFF));
-        return dr * dr + dg * dg + db * db;
+    private boolean haveSimilarColor(int rgb1, int rgb2) {
+        return (rgb1 == rgb2) || (getContrast(rgb1, rgb2) <= _similarColorMaxContrast);
     }
+
 }
